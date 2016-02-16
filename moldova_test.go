b8859ea8ede30bb37eda8b0848c3d7392960ea96
StabbyCutyou <smattkelly@gmail.com>
2016-02-15 21:53:47 -0500

    Removing old test

--- moldova_test.go
+++ moldova_test.go
@@ -340,27 +340,6 @@ func TestAllCases(t *testing.T) {
 	}
 }
 
-func TestInteger(t *testing.T) {
-	template := "{int:min:5|max:6}"
-	cs, err := BuildCallstack(template)
-	if err != nil {
-		t.Error(err)
-	}
-	result := &bytes.Buffer{}
-	err = cs.Write(result)
-	if err != nil {
-		t.Error(err)
-	}
-
-	c, err := strconv.Atoi(result.String())
-	if err != nil {
-		t.Error(err)
-	}
-	if c < 5 || c > 6 {
-		t.Error("Integer out of range")
-	}
-}
-
 func BenchmarkWrites(b *testing.B) {
 	template := "INSERT INTO floof VALUES ('{guid}','{time},'{guid:ordinal:0}','{country}',{int:min:-2000|max:0},{int:min:100|max:1000},{float:min:-1000.0|max:-540.0},{int:min:1|max:40},'{now}','{now:ordinal:0}','{unicode:length:2|case:up}',NULL,-3)"
 	var cs *Callstack
